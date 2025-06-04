5521e4c0a04687dc84497aea3bd8f12a941edaad
Mohammad Ajam <mohammad.wael.s.ajam@gmail.com>
2025-06-04 11:41:13 +0300

    Handle the requests from the user to the chat manager
    I added the handling of all chat manager requests.
    
    Now the client can make a new user account or delete it.
    Log in an Log out.
    Create a new chat and join it as the owner of the chat, and delete the
    chat if the request is from the owner.
    Join the chat of another user and send messages.

--- server/chat.go
+++ server/chat.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 	"sync"
 
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/mattn/go-sqlite3"
 )
 
 const DatabasePath string = "sdig.db"
@@ -16,12 +16,14 @@ const DatabasePath string = "sdig.db"
 //	chatId: a unique name for each chat.
 //	chatName: the public name of the chat that is displayed.
 //	chatChan: the channel that receives the requests from users that are logged in to the chat.
+// 	owner: a string of the username of the owner of the chat.
 // 	users: a map of where the key the username and the value is a pointer to the user. note that the users in this map are not all users added to the chat in the database but only the connected to the chat.
 //	mu: a pointer to a shared mutex.
 type Chat struct {
 	chatId string				// a unique name for each chat.
 	chatName string 			// the public name of the chat that is displayed.
 	chatChan chan ClientRequest	// the channel that receives the requests from users that are logged in to the chat.
+	owner string				// the username of the owner of the chat.
 	users map[string]*User		// a map of where the key the username and the value is a pointer to the user. Note that the users in this map are not all users added to the chat but only the connected to the chat.
 	mu *sync.RWMutex			// a pointer to a shared mutex.
 }
@@ -36,21 +38,21 @@ func LoadChats(mu *sync.RWMutex) map[string]Chat {
 	}
 	defer db.Close()
 
-	rows, err := db.Query("SELECT chatId, chatName FROM chats;")
+	rows, err := db.Query("SELECT chatId, chatName, owner FROM chats;")
 	if err != nil {
 		log.Fatalln("ERROR: COULD NOT QUERY CHATS:", err)
 	}
 	defer rows.Close()
 
 	for rows.Next() {
-		var chatId, chatName string
+		var chatId, chatName, owner string
 
-		err := rows.Scan(&chatId, &chatName)
+		err := rows.Scan(&chatId, &chatName, &owner)
 		if err != nil {
 			log.Fatalln("ERROR: COULD NOT READ ROW:", err)
 		}
 		
-		chats[strings.TrimSpace(chatId)] = NewChat(strings.TrimSpace(chatId), strings.TrimSpace(chatName), mu)
+		chats[strings.TrimSpace(chatId)] = NewChat(chatId, chatName, owner, mu)
 	}
 	
 	err = rows.Err()
@@ -62,11 +64,12 @@ func LoadChats(mu *sync.RWMutex) map[string]Chat {
 }
 
 // Creates a chat object from the input.
-func NewChat(chatId string, chatName string, mu *sync.RWMutex) Chat {
+func NewChat(chatId string, chatName string, owner string, mu *sync.RWMutex) Chat {
 	return Chat {
 		chatId: chatId,
 		chatName: chatName,
 		chatChan: make(chan ClientRequest),
+		owner: owner,
 		users: make(map[string]*User),
 		mu: mu,
 	}
@@ -95,6 +98,7 @@ func (chat *Chat) HandleRequests() {
 			chat.mu.Unlock()
 			if err != nil {
 				log.Println("Error: Could not insert message", err)
+				req.sender.messages <- NewMessage("e", "An error occured")
 				continue
 			}
 			
@@ -103,6 +107,14 @@ func (chat *Chat) HandleRequests() {
 					user.conn.Write(message)
 				}
 			}
+
+		case DeleteChatRequestType:
+			for _, user := range chat.users {
+				delete(user.chats, chat.chatId)
+				user.messages <- NewMessage("n", chat.chatId + " got deleted")
+			}
+			return
+
 		case QuitRequestType:
 			delete(chat.users, req.content)
 		}
@@ -134,7 +146,7 @@ type ChatManager struct {
 func NewChatManager(mu *sync.RWMutex) ChatManager {
 	return ChatManager{
 		chats: LoadChats(mu),
-		ManagerChan: make(chan ClientRequest),
+		ManagerChan: make(chan ClientRequest, 10),
 		mu: mu,
 	}
 }
@@ -154,48 +166,102 @@ func (cm *ChatManager) HandleRequests() {
 	}
 	defer db.Close()
 
+	_, err = db.Exec("PRAGMA foreign_keys = ON;")
+	if err != nil {
+		log.Fatalln("ERROR: COULD NOT ENABLE foreign_keys:", err)
+	}
 
-	get_chats, err := db.Prepare("SELECT chatId FROM joined WHERE username=?")
+	getChats, err := db.Prepare("SELECT chatId FROM joined WHERE username=?")
 	if err != nil {
 		log.Fatalln("ERROR: FAILED TO PREPARE STATEMENT:", err)
 	}
-	defer get_chats.Close()
+	defer getChats.Close()
 
-	get_user, err := db.Prepare("SELECT name, password FROM users WHERE username=?")
+	getUser, err := db.Prepare("SELECT name, password FROM users WHERE username=?")
 	if err != nil {
 		log.Fatalln("ERROR: FAILED TO PREPARE STATEMENT:", err)
 	}
-	defer get_user.Close()
+	defer getUser.Close()
 
-	// TODO: add other sql statements such as (add_to_chat, remove_from_chat, new_chat, delete_chat)
+	addUser, err := db.Prepare("INSERT INTO users (username, name, password) VALUES (?, ?, ?)")
+	if err != nil {
+		log.Fatalln("ERROR: FAILED TO PREPARE STATEMENT:", err)
+	}
+	defer addUser.Close()
+
+	deleteUser, err := db.Prepare("DELETE FROM users where username = ? and password = ?")
+	if err != nil {
+		log.Fatalln("ERROR: FAILED TO PREPARE STATEMENT:", err)
+	}
+	defer deleteUser.Close()
+
+	getChat, err := db.Prepare("SELECT chatName, password FROM chats WHERE chatId = ?")
+	if err != nil {
+		log.Fatalln("ERROR: FAILED TO PREPARE STATEMENT:", err)
+	}
+	defer getChat.Close()
+
+	joinChat, err := db.Prepare("INSERT INTO joined (username, chatId) VALUES (?, ?)")
+	if err != nil {
+		log.Fatalln("ERROR: FAILED TO PREPARE STATEMENT:", err)
+	}
+	defer joinChat.Close()
+
+	leaveChat, err := db.Prepare("DELETE FROM joined WHERE username = ? and chatId = ?")
+	if err != nil {
+		log.Fatalln("ERROR: FAILED TO PREPARE STATEMENT:", err)
+	}
+	defer leaveChat.Close()
+
+	addChat, err := db.Prepare("INSERT INTO chats (chatId, chatName, password, owner) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		log.Fatalln("ERROR: FAILED TO PREPARE STATEMENT:", err)
+	}
+	defer addChat.Close()
+
+	getOwner, err := db.Prepare("SELECT owner FROM chats WHERE chatId = ?")
+	if err != nil {
+		log.Fatalln("ERROR: FAILED TO PREPARE STATEMENT:", err)
+	}
+	defer getOwner.Close()
+
+	deleteChat, err := db.Prepare("DELETE FROM chats WHERE chatId = ? and password = ?")
+	if err != nil {
+		log.Fatalln("ERROR: FAILED TO PREPARE STATEMENT:", err)
+	}
+	defer deleteChat.Close()
+
 	// NOTE: should try adding other ones such as (rename_chat, change_password)
 
 	for {
 		req := <- cm.ManagerChan
 
-		log.Println("Got Request")
-
 		switch req.string {
 		case LoginRequestType:
-			cm.mu.RLock()
 			username, sentPassword, _ := strings.Cut(req.content, " ")
 
 			var password string
 			var nickname string
 
 			username = strings.TrimSpace(username)
-			err := get_user.QueryRow(username).Scan(&nickname, &password)
+			cm.mu.RLock()
+			err := getUser.QueryRow(username).Scan(&nickname, &password)
+			cm.mu.RUnlock()
 			if err == sql.ErrNoRows {
 				req.sender.messages <- NewMessage("e", "NoSuchUser")
 			} else if err != nil {
 				log.Println("Error: Could not search for user", err)
+				req.sender.messages <- NewMessage("e", "An error occured")
 			}
 
 			sentPassword = strings.TrimSpace(sentPassword)
 			if password == sentPassword {
-				rows, err := get_chats.Query(strings.TrimSpace(username))
+				cm.mu.RLock()
+				rows, err := getChats.Query(strings.TrimSpace(username))
+				cm.mu.RUnlock()
 				if err != nil {
 					log.Println("Error: Unable to query logged_in table:", err)
+					req.sender.messages <- NewMessage("e", "An error occured")
 				}
 
 				for rows.Next() {
@@ -204,6 +270,7 @@ func (cm *ChatManager) HandleRequests() {
 					err := rows.Scan(&chatId)
 					if err != nil {
 						log.Println("Error: Unable to scan row:", err)
+						req.sender.messages <- NewMessage("e", "An error occured")
 					}
 					req.sender.chats[chatId] = cm.chats[chatId].chatChan
 					cm.chats[chatId].users[username] = req.sender
@@ -213,7 +280,205 @@ func (cm *ChatManager) HandleRequests() {
 				req.sender.connected = true
 				req.sender.messages <- NewMessage("n", "connected")
 			}
+
+		case NewUserRequestType:
+			parts:= strings.Split(req.content, " ")
+			numParts := len(parts)
+			username := parts[0]
+			name := strings.Join(parts[1:numParts-1], " ")
+			password := parts[numParts-1]
+
+			cm.mu.Lock()
+			_, err = addUser.Exec(username, name, password)
+			cm.mu.Unlock()
+			if err != nil {
+				if sqliteErr, ok := err.(sqlite3.Error); ok {
+					if sqliteErr.Code == sqlite3.ErrConstraint {
+						req.sender.messages <- NewMessage("n", "Username already taken")
+						continue
+					}
+				}
+				log.Println("Error: Could not add user to users table", err)
+				req.sender.messages <- NewMessage("e", "An error occured")
+			}
+
+			req.sender.username = username
+			req.sender.name = name
+			req.sender.connected = true
+			req.sender.messages <- NewMessage("n", "User Created and logged in")
+
+		case DeleteUserRequestType:
+			password := req.content
+			cm.mu.Lock()
+			res, err := deleteUser.Exec(req.sender.username, password)
+			cm.mu.Unlock()
+			if sqliteErr, ok := err.(sqlite3.Error); ok {
+				if sqliteErr.Code == sqlite3.ErrNo(sqlite3.ErrConstraint) {
+					req.sender.messages <- NewMessage("n", "You are the owner of at least one chat, delete or transfer ownership of the chats first.")
+					continue
+				}
+			} else if err != nil {
+				log.Println("Error: Could not delete user:", err)
+				req.sender.messages <- NewMessage("e", "An error occured")
+				continue
+			}
+
+			affected, err := res.RowsAffected()
+			if err != nil {
+				log.Println("Error: Could not get affected rows number:", err)
+			}
+
+			if affected != 0 {
+				req.sender.username = ""
+				req.sender.chats = make(map[string]chan ClientRequest)
+				req.sender.connected = false
+				req.sender.messages <- NewMessage("n", "User deleted")
+			}
+		
+		case JoinChatRequestType:
+			chatId, sentChatPassword, _ := strings.Cut(req.content, " ")
+
+			var chatName string
+			var chatPassword string
+
+			chatId = strings.TrimSpace(chatId)
+			cm.mu.RLock()
+			err := getChat.QueryRow(chatId).Scan(&chatName, &chatPassword)
 			cm.mu.RUnlock()
+			if err == sql.ErrNoRows {
+				req.sender.messages <- NewMessage("e", "No Such Chat")
+				continue
+			} else if err != nil {
+				log.Println("Error: Could not search for user", err)
+				req.sender.messages <- NewMessage("e", "An error occured")
+				continue
+			}
+
+			sentChatPassword = strings.TrimSpace(sentChatPassword)
+			if sentChatPassword == chatPassword {
+				cm.mu.Lock()
+				res, err := joinChat.Exec(req.sender.username, chatId)
+				cm.mu.Unlock()
+				if err != nil {
+					log.Println("Error: Could not join user to chat:", err)
+					req.sender.messages <- NewMessage("e", "An error occured")
+				}
+
+				affected, err := res.RowsAffected()
+				if err != nil {
+					log.Println("Error: Could not get affected rows number:", err)
+					req.sender.messages <- NewMessage("e", "An error occured")
+				}
+
+				if affected == 0 {
+					req.sender.messages <- NewMessage("n", "Could not join, probably already joined")
+				} else if affected == 1 {
+					req.sender.messages <- NewMessage("n", chatId)
+					req.sender.chats[chatId] = cm.chats[chatId].chatChan
+					cm.chats[chatId].users[req.sender.username] = req.sender
+				}
+			}
+
+		case LeaveChatRequestType:
+			chatId := req.content
+			
+			var owner string
+			cm.mu.RLock()
+			err := getOwner.QueryRow(chatId).Scan(&owner)
+			cm.mu.RUnlock()
+			if err != nil {
+				log.Println("Error: Could not leave chat:", err)
+				req.sender.messages <- NewMessage("e", "An error occured")
+				continue
+			}
+
+			if owner == req.sender.username {
+				req.sender.messages <- NewMessage("n", "You are the owner of the chat, transfer the ownership of the chat or delete the chat.")
+				continue
+			}
+
+			cm.mu.Lock()
+			res, err := leaveChat.Exec(req.sender.username, chatId)
+			cm.mu.Unlock()
+			if err != nil {
+				log.Println("Error: Could not leave chat:", err)
+				req.sender.messages <- NewMessage("e", "An error occured")
+				continue
+			}
+
+			affected, err := res.RowsAffected()
+			if err != nil {
+				log.Println("Error: Could not get affected rows number:", err)
+				req.sender.messages <- NewMessage("e", "An error occured")
+				continue
+			}
+
+			if affected != 0 {
+				delete(req.sender.chats, chatId)
+				delete(cm.chats[chatId].users, req.sender.username)
+				req.sender.messages <- NewMessage("n", "Left " + chatId)
+			}
+
+		case NewChatRequestType:
+			parts:= strings.Split(req.content, " ")
+			numParts := len(parts)
+			chatId := parts[0]
+			chatName := strings.Join(parts[1:numParts-1], " ")
+			password := parts[numParts-1]
+
+			cm.mu.Lock()
+			_, err = addChat.Exec(chatId, chatName, password, req.sender.username)
+			cm.mu.Unlock()
+			if sqliteErr, ok := err.(sqlite3.Error); ok {
+				if sqliteErr.Code == sqlite3.ErrConstraint {
+					req.sender.messages <- NewMessage("n", "ChatId already taken")
+					break
+				}
+			} else if err != nil{
+				log.Println("Error: Could not add chat to chata table", err)
+			}
+			
+			newChat := NewChat(chatId, chatName, req.sender.username, cm.mu)
+			cm.chats[chatId] = newChat
+			req.sender.chats[chatId] = newChat.chatChan
+			go newChat.HandleRequests()
+			cm.ManagerChan <- JoinChatRequest(chatId, password, req.sender)
+			req.sender.messages <- NewMessage("n", "Created new chat: " + chatId)
+
+		case DeleteChatRequestType:
+			chatId, chatPassword, _ := strings.Cut(req.content, " ")
+
+			var owner string
+			cm.mu.RLock()
+			err := getOwner.QueryRow(chatId).Scan(&owner)
+			cm.mu.RUnlock()
+			if err != nil {
+				log.Println("Error: Could not leave chat:", err)
+				req.sender.messages <- NewMessage("e", "An error occured")
+				continue
+			}
+
+			if owner != req.sender.username {
+				req.sender.messages <- NewMessage("n", "You are not the owner of the chat")
+				continue
+			}
+
+			cm.mu.Lock()
+			res, err := deleteChat.Exec(chatId, chatPassword)
+			cm.mu.Unlock()
+			if err != nil {
+				log.Println("Error: Could not delete user:", err)
+			}
+
+			affected, err := res.RowsAffected()
+			if err != nil {
+				log.Println("Error: Could not get affected rows number:", err)
+			}
+
+			if affected != 0 {
+				cm.chats[chatId].chatChan <- DeleteChatRequest(chatId, chatPassword, req.sender)
+				delete(cm.chats, chatId)
+			}
 		}
 	}
 }
